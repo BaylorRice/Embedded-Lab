e1c2b9995da75c9a148efe4cc757ebfe232d9007
Reese Ford <reese_ford1@baylor.edu>
2024-09-03 13:24:07 -0500

    Added common.h and utilities.h
    Part 1

---
+++ Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/common.h
@@ -0,0 +1,39 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+// breadboard LED macros
+#define BREADBOARD_LED_PIN EXT1_PIN_6
+#define BREADBOARD_LED_OFF 0
+#define BREADBOARD_LED_ON  1
+#define NUM_MICROCONTROLLER_PINS 100
+
+// pin level enumeration
+// "level" indicates the voltage level of
+// the input, either high or low
+typedef enum{
+	PIN_LEVEL_LOW  = 0,
+	PIN_LEVEL_HIGH = 1
+}PIN_LEVEL_TYPE;
+
+// gpio input state enumeration
+// "state" indicates the state of the input
+// when taking the previous level into account
+typedef enum{
+	GPIO_INPUT_STATE_LOW = 0,			// level is low and was previously low
+	GPIO_INPUT_STATE_RISING_EDGE = 1,	// level is high and was previously low
+	GPIO_INPUT_STATE_HIGH = 2,			// level is high and was previously high
+	GPIO_INPUT_STATE_FALLING_EDGE = 3	// level is low and was previously high
+}GPIO_INPUT_STATE_TYPE;
+
+// led state enumeration
+// this is a generic term to describe whether the
+// LED is on or off, independent of the circuitry
+// and whether 0 or 1 turns the LED on
+typedef enum{
+	LED_STATE_OFF = 0,
+	LED_STATE_ON  = 1
+}LED_STATE_TYPE;
+
+
+
+#endif /* COMMON_H_ */
---
+++ Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/utilities.h
@@ -0,0 +1,60 @@
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+#include "common.h"
+
+// create function prototypes based on the function definitions below
+GPIO_INPUT_STATE_TYPE check_gpio_input_state(int);
+
+/////////////////////////////////////////////////////////////////////////////
+// Name: check_gpio_input_state
+// Inputs: int pin_number - the pin number of the gpio input pin
+// Outputs: GPIO_INPUT_STATE_TYPE gpio_input_state
+// See definition of GPIO_INPUT_STATE_TYPE in common.h for possible values
+// IMPORTANT TERMINOLOGY NOTE: the term "level" simply indicates the 0 or 1
+// level of the input pin.  The term "state" is more complex.  It indicates
+// the state of the input pin such as FALLING EDGE, LOW, RISING EDGE, or HIGH
+// the state takes into account history, whereas level is just a 0 or 1
+/////////////////////////////////////////////////////////////////////////////
+GPIO_INPUT_STATE_TYPE check_gpio_input_state(int pin_number)
+{
+	// create static variables for current and previous pin input levels
+	// Note on static variables: static variables retain their values even after
+	// the function exits because they are stored in the static section of memory
+	// rather than the stack.  They are initialized to their initial values the
+	// first time the function is called, but then they are not re-initialized
+	// each time the function is called
+	static PIN_LEVEL_TYPE current_pin_level[NUM_MICROCONTROLLER_PINS];
+	static PIN_LEVEL_TYPE prev_pin_level[NUM_MICROCONTROLLER_PINS];
+	
+	// the following few lines of code are intended to be executed one time to
+	// determine the initial state of the pin.  This will prevent incorrectly
+	// detecting an edge the first time the code is run
+	static bool flags_initialized = false;
+	static bool first_time_pin[NUM_MICROCONTROLLER_PINS];
+	if(!flags_initialized)
+	{
+		for(int i=0; i < NUM_MICROCONTROLLER_PINS; i++)
+		{
+			first_time_pin[i] = true;
+		}
+		flags_initialized = true;
+	}
+	if(first_time_pin[pin_number])
+	{
+		current_pin_level[pin_number] = ioport_get_pin_level(pin_number);
+		prev_pin_level[pin_number] = current_pin_level[pin_number];
+		first_time_pin[pin_number] = false;
+	}
+	
+	// create and initialize gpio_input_state
+	// the initialization value is arbitrary because it will be
+	// updated by the logic of the function
+	GPIO_INPUT_STATE_TYPE gpio_input_state = GPIO_INPUT_STATE_LOW;
+	
+	// TODO: Add logic here
+	
+	return gpio_input_state;
+}
+
+#endif /* UTILITIES_H_ */
