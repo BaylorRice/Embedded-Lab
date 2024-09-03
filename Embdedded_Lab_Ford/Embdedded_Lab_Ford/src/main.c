4116c865f19a2729f3e0f31e8a3d8264bdf54ab0
Reese Ford <reese_ford1@baylor.edu>
2024-09-03 13:23:47 -0500

    Added breadboard LED to main.c
    Part 1

--- Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/main.c
+++ Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/main.c
@@ -31,23 +31,50 @@
 /*
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
+
 #include <asf.h>
+#include "common.h"
+#include "utilities.h"
+
+#define ONE_BUTTON_TWO_LED
+//#define TWO_BUTTON_TWO_LED
+//#define DOUBLE_FLASHLIGHT
 
 int main (void)
 {
 	board_init();
+	#ifdef ONE_BUTTON_TWO_LED
+	// configure BREADBOARD_LED_PIN
+	ioport_set_pin_dir(BREADBOARD_LED_PIN, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_level(BREADBOARD_LED_PIN, LED_0_INACTIVE);
 
-	/* Insert application code here, after the board has been initialized. */
-
-	/* This skeleton code simply sets the LED to the state of the button. */
-	while (1) {
-		/* Is button pressed? */
-		if (ioport_get_pin_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
-			/* Yes, so turn LED on. */
+	while (1)
+	{
+		// Is button pressed?
+		if (ioport_get_pin_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE)
+		{
+			// Yes, so turn LED on
 			ioport_set_pin_level(LED_0_PIN, LED_0_ACTIVE);
-		} else {
-			/* No, so turn LED off. */
+
+			// Yes, so set EXT1_PIN_6 to same value as LED
+			ioport_set_pin_level(BREADBOARD_LED_PIN, BREADBOARD_LED_ON);
+
+		}
+		else
+		{
+			// No, so turn LED off
 			ioport_set_pin_level(LED_0_PIN, !LED_0_ACTIVE);
+
+			// Yes, so set EXT1_PIN_6 to same value as LED
+			ioport_set_pin_level(BREADBOARD_LED_PIN, BREADBOARD_LED_OFF);
 		}
 	}
+	#endif
+	
+	#ifdef TWO_BUTTON_TWO_LED
+
+	#endif
+	#ifdef DOUBLE_FLASHLIGHT
+
+	#endif
 }
