8c0e9dd307734d1924d5adc3e0d63d76a70169df
Reese Ford <reese.w.ford@gmail.com>
2024-09-10 15:41:35 -0500

    Removed test line

--- Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/main.c
+++ Embdedded_Lab_Ford/Embdedded_Lab_Ford/src/main.c
@@ -114,10 +114,6 @@ int main (void)
 	GPIO_INPUT_STATE_TYPE sw0_state = GPIO_INPUT_STATE_HIGH;
 	GPIO_INPUT_STATE_TYPE sw1_state = GPIO_INPUT_STATE_LOW;
 	
-	uint32_t test_value = 0;
-	display_clock_time(test_value);
-	return 0;
-	
 	while (1) {
 		// Check for input events
 		sw0_state = check_gpio_input_state(BUTTON_0_PIN);
