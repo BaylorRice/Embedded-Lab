ccacccf2ebdc6476db1fc632992638f4bf831bdf
Reese Ford <reese.w.ford@gmail.com>
2024-10-22 13:06:44 -0500

    Update slope_calculator_list.py

--- Python Problems/slope_calculator_list.py
+++ Python Problems/slope_calculator_list.py
@@ -2,7 +2,7 @@
 # Reese Ford
 # Created 10/22/2024
 # Modified 10/22/2024 by Reese Ford
-# Last Commit: none
+# Last Commit: d5a0e1317efeae0541e263797fac4f62ab1be223
 
 def calc_slope_list(elementList):
     return (elementList[0] - elementList[3]) / elementList[2]
@@ -10,8 +10,11 @@ def calc_slope_list(elementList):
 lineList1 = [28, 999, 3, 5]
 lineList2 = [135, 888, 21, -325]
 
-lineList1[1] = calc_slope_list(lineList1)
-lineList2[1] = calc_slope_list(lineList2)
+slope1 = calc_slope_list(lineList1)
+slope2 = calc_slope_list(lineList2)
+
+lineList1[1] = slope1 # type: ignore
+lineList2[1] = slope2 # type: ignore
 
 print("Slope of List 1 =", lineList1[1])
 print("Slope of Line 2 =", lineList2[1])
