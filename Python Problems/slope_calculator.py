d64f677ed60f1fff0ebef7ccbd22db69ad0c6edc
Reese Ford <reese_ford1@baylor.edu>
2024-10-22 12:20:32 -0500

    Create slope_calculator.py
    Problem 1

---
+++ Python Problems/slope_calculator.py
@@ -0,0 +1,11 @@
+# Slope Calculator - Python Practice Problems
+# Reese Ford
+# Created 10/22/2024
+# Modified 10/22/2024 by Reese Ford
+# Last Commit: none
+
+def calc_slope(y, x, b) :
+    return (x / y)
+
+slope = calc_slope(28,3,5)
+print("Slope =", slope)
