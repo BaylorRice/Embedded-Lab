1a8ec7fa887b941a65adbc8250ac4fd429840fa2
Reese Ford <reese_ford1@baylor.edu>
2024-10-22 12:53:07 -0500

    Things being annoying

--- Python Problems/slope_calculator_list.py
+++ Python Problems/slope_calculator_list.py
@@ -5,4 +5,10 @@
 # Last Commit: none
 
 def calc_slope_list(elementList):
-    
+    return (elementList[0] - elementList[3]) / elementList[2]
+
+lineList1 = [28, 999, 3, 5]
+lineList2 = [135, 888, 21, -325]
+
+lineList1[1] = calc_slope_list(lineList1)
+lineList2[1] = calc_slope_list(lineList2)
