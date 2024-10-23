c9bf424f65b4a8e8935d35d61b04f1327410adf5
Reese Ford <reese_ford1@baylor.edu>
2024-10-22 20:50:34 -0500

    Updated last commit

--- RasPi - Python/test.py
+++ RasPi - Python/test.py
@@ -2,6 +2,6 @@
 # Reese Ford
 # Created 10/22/2024
 # Modified 10/22/2024 by Reese Ford
-# Last Commit: none
+# Last Commit: def2e0ee3dfb6635aa673b7159e33a2086b290c4
 
 print("Hello, World! Its me. Pi!")
