3bfa185dfc4e08556171dd782e7fa419a459bc2b
Reese Ford <reese_ford1@baylor.edu>
2024-10-24 13:14:53 -0500

    Implemented event detection

--- RasPi - Python/Lab RPi1 - Flashlight/flashlight_event_detect.py
+++ RasPi - Python/Lab RPi1 - Flashlight/flashlight_event_detect.py
@@ -21,7 +21,24 @@ try:
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BUTTON_0_PIN, GPIO.IN)
     GPIO.setup(LED_0_PIN, GPIO.OUT)
+    
+    def button_callback(channel):
+        print("Entering Callback")
+        GPIO.output(LED_0_PIN, True)
+        while GPIO.input(BUTTON_0_PIN) == True:
+            print("abc")
+            time.sleep(0.01)
+        GPIO.output(LED_0_PIN, False)
+        print("Exiting Callback")
 
+    GPIO.add_event_detect(BUTTON_0_PIN, GPIO.RISING, bouncetime=10)
+    GPIO.add_event_callback(BUTTON_0_PIN, button_callback)
+
+    count = 0
+    while True:
+        time.sleep(1)
+        print(count)
+        count = count + 1
 
 except KeyboardInterrupt:
     print("Got Keyboard Interrupt. Cleaning up and exiting")
