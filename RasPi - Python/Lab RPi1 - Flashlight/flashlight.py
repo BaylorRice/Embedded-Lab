ed4cff7c9b02dcff3a9cd8a02de4e662190313f3
Reese Ford <reese_ford1@baylor.edu>
2024-10-24 12:32:06 -0500

    Tested Button Input

--- RasPi - Python/Lab RPi1 - Flashlight/flashlight.py
+++ RasPi - Python/Lab RPi1 - Flashlight/flashlight.py
@@ -4,3 +4,23 @@
 # Modified 10/24/2024 by Reese Ford
 # Last Commit: none
 
+# Pin Vars
+BUTTON_0_PIN = 16
+LED_0_PIN = 18
+
+# Importing and setting up GPIO
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO")
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUTTON_0_PIN, GPIO.IN)
+
+while (1):
+    if GPIO.input(BUTTON_0_PIN):
+        print("Input was HIGH")
+    else:
+        print("Input was LOW")
+
+
