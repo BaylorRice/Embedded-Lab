42597b37f3ae9f1af950678fe9f2d6fbdbec07a6
Reese Ford <reese_ford1@baylor.edu>
2024-10-29 13:37:03 -0500

    Read ADC function

--- RasPi - Python/Lab RPi2 - Photodimmer/adc_test/adc_test.py
+++ RasPi - Python/Lab RPi2 - Photodimmer/adc_test/adc_test.py
@@ -15,4 +15,15 @@ import spidev
 # Initialize SPI
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 1000000
+def read_adc() :
+    # Configure ADC
+    configureADCPacket = [0x1, 0x80, 0x0]
+    recieved = spi.xfer(configureADCPacket)
+
+    # Convert to ADC Value
+    recieved[1] = recieved[1] << 8
+    adcValue = recieved[1] + recieved[2]
+
+    # Convert to Voltage
+    voltage = (adcValue / (pow(2,10) - 1)) * 3.3
+    return voltage
