acfb4b518b0f311b940b5d1816ac34b3038bfd04
Reese Ford <reese_ford1@baylor.edu>
2024-10-29 12:34:24 -0500

    Initialize SPI

--- RasPi - Python/Lab RPi2 - Photodimmer/adc_test/adc_test.py
+++ RasPi - Python/Lab RPi2 - Photodimmer/adc_test/adc_test.py
@@ -5,9 +5,14 @@
 # Last Commit: 
 
 # Pin Vars
-
+PIN_SCLK = 23
+PIN_MISO = 21
+PIN_MOSI = 19
+PIN_CS = 24
 
 import spidev
 
+# Initialize SPI
 spi = spidev.SpiDev()
-spi.open()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
