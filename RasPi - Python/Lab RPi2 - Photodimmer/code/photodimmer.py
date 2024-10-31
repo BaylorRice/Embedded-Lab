e50bf2699545f8336dff86408b0f07e030477b6b
Reese Ford <reese_ford1@baylor.edu>
2024-10-31 13:23:34 -0500

    Implemented Calibration

--- RasPi - Python/Lab RPi2 - Photodimmer/code/photodimmer.py
+++ RasPi - Python/Lab RPi2 - Photodimmer/code/photodimmer.py
@@ -26,17 +26,58 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO")
 
-
 # Setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_0_PIN, GPIO.IN)
 
 pwm = HW_PWM(2000)
 
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 100000
+
+def read_adc() :
+    # Configure ADC
+    configureADCPacket = [0x1, 0x80, 0x0]
+    recieved = spi.xfer(configureADCPacket)
+
+    # Convert to ADC Value
+    recieved[1] = recieved[1] << 8
+    adcValue = recieved[1] + recieved[2]
+
+    return adcValue
+
 try:
-    # Bunch of Stuff
-    pass
-except:
+    # Calibration - Covered (max)
+    print(" ")
+    print("Cover the photosensor and press the push button")
+    while True:
+        channel = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, timeout = 1000, bouncetime=10)
+        if channel == BUTTON_0_PIN:
+            max_adcValue = read_adc()
+
+            channel = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.FALLING, timeout=1000, bouncetime=10)
+            break
+
+    # Calibration - Bright (min)
+    print("Shine a flashlight on the photosensor and press the push button")
+    while True:
+        channel = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, timeout = 1000, bouncetime=10)
+        if channel == BUTTON_0_PIN:
+            min_adcValue = read_adc()
+
+            channel = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.FALLING, timeout=1000, bouncetime=10)
+            break
+    
+    print("Max:", max_adcValue, "  Min:", min_adcValue)
+
+    # Photodimmer
+    while True:
+        adcValue = read_adc()
+        print(adcValue)
+
+
+except KeyboardInterrupt:
     print('Got Keyboard Interrupt. Cleaning up and exiting')
     pwm.set_duty_cycle(0.0)
     GPIO.cleanup()
