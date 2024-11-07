0390cc2462b8e5c4d7f3b6f589bf9b06a410acda
Reese Ford <reese_ford1@baylor.edu>
2024-11-07 13:24:56 -0600

    Setup controller script

--- RasPi - Python/Lab RPi3 - Fan Control/fan_controller_monitor.py
+++ RasPi - Python/Lab RPi3 - Fan Control/fan_controller_monitor.py
@@ -4,3 +4,23 @@
 # Modified 11/07/2024 by Reese Ford
 # Last Commit: 
 
+# Import Modules
+import time
+import sys
+import os
+
+sys.path.insert(0, 'utilities')
+import utilities
+
+# Setup PWM
+pwm = utilities.HW_PWM(25000)
+pwm.set_duty_cycle(100.0)
+
+try:
+    # Stuff
+    pass
+
+except KeyboardInterrupt:
+    print('Got Keyboard Interrupt. Cleaning up and exiting')
+    pwm.set_duty_cycle(100.0)
+    sys.exit()
