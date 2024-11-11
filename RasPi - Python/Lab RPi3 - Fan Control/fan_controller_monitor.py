8a1fedceeae1cf55c5f16dbdb8ba2848098c8ebe
Reese Ford <reese_ford1@baylor.edu>
2024-11-11 12:19:14 -0600

    Date Modified

--- RasPi - Python/Lab RPi3 - Fan Control/fan_controller_monitor.py
+++ RasPi - Python/Lab RPi3 - Fan Control/fan_controller_monitor.py
@@ -1,7 +1,7 @@
 # Fan Controller Monitor - Lab RPi3 Fan Controller
 # Reese Ford
 # Created 11/07/2024
-# Modified 11/07/2024 by Reese Ford
+# Modified 11/11/2024 by Reese Ford
 # Last Commit: 
 
 # Import Modules
