c1effa160bd933799875faefc4fdd71cb19526da
Reese Ford <reese_ford1@baylor.edu>
2024-11-19 12:00:15 -0600

    Implemented read_adc in weather station

--- RasPi - Python/application_exam/weather_station.py
+++ RasPi - Python/application_exam/weather_station.py
@@ -3,3 +3,40 @@
 # Created 11/19/2024
 # Modified 11/19/2024 by Reese Ford
 # Last Commit: 
+
+# Pin Vars
+PIN_SCLK = 23
+PIN_MISO = 21
+PIN_MOSI = 19
+PIN_CS = 24
+
+import spidev
+import time
+import sys
+
+# Initialize SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 100000
+
+def read_adc() :
+    # Configure ADC
+    configureADCPacket = [0x1, 0x20, 0x0]
+    recieved = spi.xfer(configureADCPacket)
+
+    # Convert to ADC Value
+    recieved[1] = recieved[1] << 8
+    adcValue = recieved[1] + recieved[2]
+
+    # Convert to Voltage
+    voltage = (adcValue / (pow(2,10) - 1)) * 3.3
+    return voltage
+
+try: 
+    while True:
+        voltage = read_adc()
+        print(voltage)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print(" Stopping Program")
+    sys.exit()
