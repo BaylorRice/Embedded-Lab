06f99856b3a1e168213c8ef9efaf8a108bd7d22f
Reese Ford <reese_ford1@baylor.edu>
2024-11-19 12:05:29 -0600

    Implemented temperature calculation

--- RasPi - Python/application_exam/weather_station.py
+++ RasPi - Python/application_exam/weather_station.py
@@ -19,10 +19,25 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 100000
 
-def read_adc() :
+def read_temp() :
     # Configure ADC
     configureADCPacket = [0x1, 0x20, 0x0]
     recieved = spi.xfer(configureADCPacket)
+
+    # Convert to ADC Value
+    recieved[1] = recieved[1] << 8
+    adcValue = recieved[1] + recieved[2]
+
+    # Convert to Voltage
+    voltage = (adcValue / (pow(2,10) - 1)) * 3.3
+    
+    # Convert to Temperature (C)
+    temp_c = 100*(voltage) - 50
+
+    # Convert to Temperature (F)
+    temp_f = (9/5)*temp_c + 32
+    return temp_f
+
 
     # Convert to ADC Value
     recieved[1] = recieved[1] << 8
